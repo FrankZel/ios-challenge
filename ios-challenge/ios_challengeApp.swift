6edd3632c67de85d0d13415eafb2e88f4584fd28
kc-frankzel <franco.zelener@kinandcarta.com>
2024-05-19 13:40:24 -0300

    Initial Commit

---
+++ ios-challenge/ios_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_challengeApp.swift
+//  ios-challenge
+//
+//  Created by Franco Zelener on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
